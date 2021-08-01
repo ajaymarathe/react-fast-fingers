db06fde3b24226932e63adf3bde40d21c4d241b0
Ajay Marathe <ma143ajay@gmail.com>
2021-08-01 10:30:06 +0530

    added input and word container box.

--- src/components/WordToType.js
+++ src/components/WordToType.js
@@ -4,14 +4,26 @@ class WordToType extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      inputValue: '',
     };
   }
 
   render() {
     return (
       <div>
-        <div className='wordContainer'>
-          <p>fast fingers.</p>
+        <div className='row justify-content-center'>
+          <div className='col-md-8'>
+            <div className='wordContainer'>
+              <p>World</p>
+            </div>
+            <div className='inputContainer'>
+              <input className='form-control' type="text" onChange={(event) => {
+                this.setState({ inputValue: event.target.value})
+              }}
+              value={this.state.inputValue}
+               />
+            </div>
+          </div>
         </div>
       </div>
     );
