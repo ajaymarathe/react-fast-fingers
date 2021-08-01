8ecde655abd2eb879d6fb9fddec01e1ce1dcc7d1
Ajay Marathe <ma143ajay@gmail.com>
2021-08-01 10:01:15 +0530

    created new WordToType component and few ui changes.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import WordToType from './components/WordToType';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello world</h2>
+    <div className="container">
+      <WordToType />
     </div>
   );
 }
---
+++ src/components/WordToType.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class WordToType extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='wordContainer'>
+          <p>fast fingers.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default WordToType;
